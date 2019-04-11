c5f0343578b3571253b70ad7e6881f1aeaa73eb0
Dzay <Dzay11@hotmail.com>
2019-04-11 18:22:44 -0500

    practica1Cambio

--- practicas1/src/practicas1/Practicas1.java
+++ practicas1/src/practicas1/Practicas1.java
@@ -4,18 +4,22 @@
  * and open the template in the editor.
  */
 package practicas1;
-
 import data.Processing;
+import java.io.File;
+import java.io.IOException;
 import templates.Persona;
+import org.apache.commons.io.input.ReversedLinesFileReader;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Practicas1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        ArrayList<Persona> personas = new ArrayList<>();
-        ArrayList<Persona> outPersonas = new ArrayList<>();
+    ArrayList<Persona> personas = new ArrayList<>();
+    ArrayList<Persona> outPersonas = new ArrayList<>();
 
         personas.add(new Persona(
                 "Pedro Rodirguez",
@@ -39,16 +43,33 @@ public class Practicas1 {
         Processing processing = new Processing();
 //        processing.guardarPersonasEnArchivo(personas, "personas.txt");
         outPersonas = processing.leerPersonasDeArchivo("personas.txt");
-        for (int i = 0; i < outPersonas.size(); i++) {
+        for (int i=0; i< outPersonas.size(); i++){
             System.out.println(
-                    outPersonas.get(i).getNombre() + " | "
-                    + outPersonas.get(i).getEdad() + " | "
-                    + outPersonas.get(i).getEstatura() + " | "
-                    + outPersonas.get(i).getPeso() + " | "
+                    outPersonas.get(i).getNombre() + " | " +
+                            outPersonas.get(i).getEdad() + " | " +
+                            outPersonas.get(i).getEstatura() + " | " +
+                            outPersonas.get(i).getPeso() + " | "
             );
         }
-    Persona perUlti= new 
-       Persona(outPersonas.get(outPersonas.size()-1).getNombre(), outPersonas.get(outPersonas.size()-1).getEdad(), outPersonas.get(outPersonas.size()-1).getEstatura(), outPersonas.get(outPersonas.size()-1).getPeso());
-        System.out.print(perUlti.toString());
+        
+        File file = new File("personas.txt");
+        ReversedLinesFileReader object = new ReversedLinesFileReader(file);
+        String line = object.readLine();
+        System.out.println(line);
+        String pattern = "\\(([^)]+)\\)\\(([^)]+)\\)\\(([^)]+)\\)\\(([^)]+)\\)";
+        Matcher matcher = Pattern.compile(pattern).matcher(line);
+        Persona persUlt = null;
+        if (matcher.find()) {
+            persUlt = new Persona(
+                                matcher.group(1),
+                                Integer.parseInt( matcher.group(2) ),
+                                Double.parseDouble( matcher.group(3) ),
+                                Integer.parseInt( matcher.group(4) )
+                        );
+        }
+        System.out.println("La última "+persUlt.toString());
+        
+
     }
+   
 }
